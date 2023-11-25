956a3d2ca87ed94ccf504594796d7cc5131980d5
Segoy <segoy.thiel@protonmail.com>
2023-11-25 09:08:31 +0100

    Assignment 2 of Basic Section of Course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import {ServerComponent} from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import {FormsModule} from "@angular/forms";
+import { UserInputComponent } from './user-input/user-input.component';
 
 @NgModule({
   declarations: [
@@ -13,9 +15,11 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     ServerComponent,
     ServersComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    UserInputComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl:'servers.component.html',
   styleUrl: './servers.component.css'
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created';
+  serverName = 'Testserver';
 
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    }, 2000);
+  }
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+  onUpdateServerName(event: any){
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
 }
